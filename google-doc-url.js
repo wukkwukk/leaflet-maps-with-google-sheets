c0aec05b98a441e4803ae522722357bc3d3635d2
wukkwukk <malte.schremmer@gmail.com>
2017-02-15 21:52:18 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1rTPwRRekEN3z6DbdLGblWrOhwSqFeTFlETwTe3uJ4xY/edit#gid=0';
